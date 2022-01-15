3deb5e5c7a6a964869c64119fee1ca214ed75ae3
Akbar Fadiansyah <akbarfadi01@gmail.com>
2022-01-15 11:05:16 +0700

    add query strings for airport frequencies

--- src/bin/web/pages/airport_frequencies.rs
+++ src/bin/web/pages/airport_frequencies.rs
@@ -1,19 +1,43 @@
-use super::{AppState, ErrorResponse};
-use actix_web::{get, web, HttpResponse, Responder};
+use std::collections::BTreeSet;
+
+use actix_web::{get, HttpResponse, Responder, web};
+use serde::Deserialize;
+
 use ourairports_api::ourairports::airport_frequencies::AirportFrequency;
 use ourairports_api::ourairports::Id;
 
-// TODO: query params:
-// airport_ref
-// airport_ident
+use super::{AppState, ErrorResponse};
+
+#[derive(Deserialize)]
+pub struct QueryParams {
+    pub airport_ref: Option<Id>,
+    pub airport_ident: Option<String>
+}
 
 #[get("/api/v1/airport-frequencies")]
-pub async fn get_airport_frequencies(data: web::Data<AppState>) -> impl Responder {
-    let body = &data
-        .airport_frequencies
-        .values()
-        .collect::<Vec<&AirportFrequency>>();
-    HttpResponse::Ok().json(body)
+pub async fn get_airport_frequencies(data: web::Data<AppState>, params: web::Query<QueryParams>) -> impl Responder {
+    if params.airport_ref.is_some() || params.airport_ident.is_some() {
+        let mut body = BTreeSet::new();
+        for freq in data.airport_frequencies.values() {
+            if let Some(airport_ref) = params.airport_ref {
+                if airport_ref == freq.airport_ref() {
+                    body.insert(freq);
+                }
+            }
+            if let Some(airport_ident) = &params.airport_ident {
+                if airport_ident.to_ascii_lowercase() == freq.airport_ident().to_ascii_lowercase() {
+                    body.insert(freq);
+                }
+            }
+        }
+        HttpResponse::Ok().json(body)
+    } else {
+        let body = &data
+            .airport_frequencies
+            .values()
+            .collect::<Vec<&AirportFrequency>>();
+        HttpResponse::Ok().json(body)
+    }
 }
 
 #[get("/api/v1/airport-frequencies/{id}")]

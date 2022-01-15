11fd158d508ad17535dc6a0008df1faf70889d9b
Akbar Fadiansyah <akbarfadi01@gmail.com>
2022-01-15 11:44:56 +0700

    add query strings for regions

--- src/bin/web/pages/regions.rs
+++ src/bin/web/pages/regions.rs
@@ -1,18 +1,46 @@
-use super::{AppState, ErrorResponse};
-use actix_web::{get, web, HttpResponse, Responder};
-use ourairports_api::ourairports::regions::Region;
-use ourairports_api::ourairports::Id;
+use std::collections::BTreeSet;
 
-// TODO: query params:
-// code
-// local_code
-// iso_country
-// name
+use actix_web::{get, HttpResponse, Responder, web};
+use serde::Deserialize;
+
+use ourairports_api::ourairports::Id;
+use ourairports_api::ourairports::regions::Region;
+
+use super::{AppState, ErrorResponse};
+
+#[derive(Deserialize)]
+pub struct QueryParams {
+    pub iso_country: Option<String>,
+    pub code: Option<String>,
+    pub local_code: Option<String>,
+}
 
 #[get("/api/v1/regions")]
-pub async fn get_regions(data: web::Data<AppState>) -> impl Responder {
-    let body = &data.regions.values().collect::<Vec<&Region>>();
-    HttpResponse::Ok().json(body)
+pub async fn get_regions(data: web::Data<AppState>, params: web::Query<QueryParams>) -> impl Responder {
+    if params.iso_country.is_some() || params.code.is_some() || params.local_code.is_some() {
+        let mut body = BTreeSet::new();
+        for region in data.regions.values() {
+            if let Some(iso_country) = &params.iso_country {
+                if iso_country.to_ascii_lowercase() == region.iso_country().to_ascii_lowercase() {
+                    body.insert(region);
+                }
+            }
+            if let Some(code) = &params.code {
+                if code.to_ascii_lowercase() == region.code().to_ascii_lowercase() {
+                    body.insert(region);
+                }
+            }
+            if let Some(local_code) = &params.local_code {
+                if local_code.to_ascii_lowercase() == region.local_code().to_ascii_lowercase() {
+                    body.insert(region);
+                }
+            }
+        }
+        HttpResponse::Ok().json(body)
+    } else {
+        let body = data.regions.values().collect::<Vec<&Region>>();
+        HttpResponse::Ok().json(body)
+    }
 }
 
 #[get("/api/v1/regions/{id}")]

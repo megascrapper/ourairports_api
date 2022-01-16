e9b39148e5d7bd6dbd6fa5051a5cbeebfd9b686c
Akbar Fadiansyah <akbarfadi01@gmail.com>
2022-01-16 16:30:10 +0700

    add countries and regions documentation

--- src/ourairports/countries.rs
+++ src/ourairports/countries.rs
@@ -1,3 +1,22 @@
+//! Contains the type representing a single country.
+//!
+//! # Examples
+//! ```
+//! use ourairports_api::ourairports::countries::*;
+//! use ourairports_api::ourairports::Continent;
+//!
+//! # fn main() -> Result<(), Box<dyn std::error::Error>> {
+//!     let countries = get_countries_csv()?;
+//!
+//!     let sample_country = countries.get(&302791).unwrap();
+//!     assert_eq!("Brazil", sample_country.name());
+//!     assert_eq!("BR", sample_country.code());
+//!     assert_eq!(&Continent::SouthAmerica, sample_country.continent());
+//!
+//! #    Ok(())
+//! # }
+//! ```
+
 use std::cmp::Ordering;
 use std::collections::BTreeMap;
 use std::hash::{Hash, Hasher};
@@ -22,21 +41,35 @@ pub struct Country {
 }
 
 impl Country {
+    /// Internal OurAirports integer identifier for the country.
+    /// This will stay persistent, even if the country name or code changes.
     pub fn id(&self) -> Id {
         self.id
     }
+    /// The two-character [ISO 3166:1-alpha2 code](https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes)
+    /// for the country.
+    ///
+    /// A handful of unofficial, non-ISO codes are also in use, such as "XK" for Kosovo.
     pub fn code(&self) -> &str {
         &self.code
     }
+    /// The common English-language name for the country.
+    /// Other variations of the name may appear in [`self.keywords()`] to assist with search.
     pub fn name(&self) -> &str {
         &self.name
     }
+    /// The continent where the country is (primarily) located
+    ///
+    /// See [`Continent`](super::Continent) for possible continent values.
     pub fn continent(&self) -> &Continent {
         &self.continent
     }
+    /// Link to the Wikipedia article about the country.
     pub fn wikipedia_link(&self) -> &str {
         &self.wikipedia_link
     }
+    /// A list of of search keywords/phrases related to the country. Each item represents one
+    /// keyword.
     pub fn keywords(&self) -> &Vec<String> {
         &self.keywords
     }
@@ -70,8 +103,12 @@ impl Hash for Country {
 
 impl ToJsonString for Country {}
 
-/// Returns a `HashMap` of countries from `countries.csv` with the country id as key and `Country`
-/// struct as value
+/// Returns a [`BTreeMap`] of all [`Country`] in the latest OurAirports `countries.csv`
+/// with its ID as the key, sorted according to its keys.
+///
+/// # Errors
+/// Returns [`FetchError`] if the data cannot be fetched or there's something wrong
+/// with the de serialization process.
 pub fn get_countries_csv() -> Result<BTreeMap<Id, Country>, FetchError> {
     // get data
     debug!("getting data");
--- src/ourairports/regions.rs
+++ src/ourairports/regions.rs
@@ -1,3 +1,22 @@
+//! Contains the type representing a single administrative subdivision.
+//!
+//! # Examples
+//! ```
+//! use ourairports_api::ourairports::regions::*;
+//! use ourairports_api::ourairports::Continent;
+//!
+//! # fn main() -> Result<(), Box<dyn std::error::Error>> {
+//!     let regions = get_regions_csv()?;
+//!
+//!     let sample_region = regions.get(&306408).unwrap();
+//!     assert_eq!("GE", sample_region.iso_country());
+//!     assert_eq!("KA", sample_region.local_code());
+//!     assert_eq!(&format!("{}-{}", sample_region.iso_country(), sample_region.local_code()), sample_region.code());
+//!
+//! #    Ok(())
+//! # }
+//! ```
+
 use std::cmp::Ordering;
 use std::collections::BTreeMap;
 use std::hash::{Hash, Hasher};
@@ -9,6 +28,7 @@ use crate::ourairports::{vec_string_from_string, Continent, FetchError, Id, ToJs
 
 const REGIONS_CSV_URL: &str = "https://davidmegginson.github.io/ourairports-data/regions.csv";
 
+/// Represents a high-level administrative subdivision of a country.
 #[derive(Deserialize, Debug, Clone, Serialize)]
 pub struct Region {
     id: Id,
@@ -23,27 +43,49 @@ pub struct Region {
 }
 
 impl Region {
+    /// Internal OurAirports integer identifier for the region.
+    /// This will stay persistent, even if the region code changes.
     pub fn id(&self) -> Id {
         self.id
     }
+    /// [`self.local_code()`] prefixed with the country code to make a globally-unique identifier.
     pub fn code(&self) -> &str {
         &self.code
     }
+    /// local_code prefixed with the country code to make a globally-unique identifier.
+    ///
+    /// Whenever possible, these are official [ISO 3166:2](https://en.wikipedia.org/wiki/ISO_3166-2),
+    /// at the highest level available, but in some cases OurAirports has to use unofficial codes.
+    /// There is also a pseudo code `U-A` for each country, which means that the airport has not yet
+    /// been assigned to a region (or perhaps can't be, as in the case of a deep-sea oil platform).
     pub fn local_code(&self) -> &str {
         &self.local_code
     }
+    /// The common English-language name for the administrative subdivision.
+    ///
+    /// In some cases, the name in local languages will appear in `self.keywords()`
     pub fn name(&self) -> &str {
         &self.name
     }
+    /// A code for the continent to which the region belongs.
+    ///
+    /// See [`Continent`](super::Continent) for possible continent values.
     pub fn continent(&self) -> &Continent {
         &self.continent
     }
+    /// The two-character [ISO 3166:1-alpha2 code](https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes)
+    /// for the country containing the administrative subdivision.
+    ///
+    /// A handful of unofficial, non-ISO codes are also in use, such as "XK" for Kosovo.
     pub fn iso_country(&self) -> &str {
         &self.iso_country
     }
+    /// A link to the Wikipedia article describing the subdivision.
     pub fn wikipedia_link(&self) -> &str {
         &self.wikipedia_link
     }
+    /// A list of keywords to assist with search. May include former names for the region, and/or
+    /// the region name in other languages. Each item represents one keyword.
     pub fn keywords(&self) -> &Vec<String> {
         &self.keywords
     }
@@ -77,6 +119,12 @@ impl Hash for Region {
 
 impl ToJsonString for Region {}
 
+/// Returns a [`BTreeMap`] of all [`Region`] in the latest OurAirports `regions.csv`
+/// with its ID as the key, sorted according to its keys.
+///
+/// # Errors
+/// Returns [`FetchError`] if the data cannot be fetched or there's something wrong
+/// with the de serialization process.
 pub fn get_regions_csv() -> Result<BTreeMap<Id, Region>, FetchError> {
     // get data
     debug!("getting data");

7546c9b65f852eeb2150bd821bd3cb914a0e2000
Akbar Fadiansyah <akbarfadi01@gmail.com>
2022-01-20 14:26:31 +0700

    increase timeout for data retrieval for slow connections

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,21 @@
 use log::debug;
+use reqwest::blocking::Client;
+use std::time::Duration;
 
 pub mod ourairports;
 
-// async fn web_request(url: &str) -> Result<String, reqwest::Error> {
-//     debug!("requesting data from {}", url);
-//     reqwest::get(url).await?.text().await
-// }
+// time limit for downloading one file
+const REQUEST_TIMEOUT: u64 = 300;
 
 fn web_request_blocking(url: &str) -> Result<String, reqwest::Error> {
     debug!("requesting data from {}", url);
-    reqwest::blocking::get(url)?.text()
+    //reqwest::blocking::get(url)?.text()
+    let client = Client::builder()
+        .timeout(Duration::from_secs(REQUEST_TIMEOUT))
+        .build()?;
+    client.get(url).send()?.text()
+
+
 }
 
 #[cfg(test)]
